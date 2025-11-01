b3ebb41f226cef3c199bba287c0eb7e535013335
Joseph Langstroth <jlangstroth@shaw.ca>
2025-11-01 08:40:31 -0700

    Initial Commit

---
+++ FlightRadar/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FlightRadar
+//
+//  Created by Joseph Langstroth on 2025-11-01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FlightRadar/FlightRadarApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlightRadarApp.swift
+//  FlightRadar
+//
+//  Created by Joseph Langstroth on 2025-11-01.
+//
+
+import SwiftUI
+
+@main
+struct FlightRadarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
